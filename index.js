53a6f13e225a729b7833a2a9886c4c574f9ba702
RachelHumphrey <rachelhumphrey87@gmail.com>
2023-03-19 19:06:28 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, value) {
+    let messages = []
+    for (let i = 0; i <names.length; i++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+     } 
+       return messages; 
+    }
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0 ) {
+        console.log(countDown--)
+    }
+}
+//* logs each number when counting down
